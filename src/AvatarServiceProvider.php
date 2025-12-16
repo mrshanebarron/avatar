e7bdd3a3ae14008702025c7a9905fb48b2060499
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 19:25:32 -0500

    Fix service provider filename case

--- src/AvatarServiceProvider.php
+++ src/AvatarServiceProvider.php
@@ -6,11 +6,23 @@ use Illuminate\Support\ServiceProvider;
 
 class AvatarServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-avatar.php', 'sb-avatar');
+    }
+
     public function boot(): void
     {
         if (class_exists(\Livewire\Livewire::class)) {
             \Livewire\Livewire::component('sb-avatar', Livewire\Avatar::class);
         }
+        
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-avatar');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-avatar.php' => config_path('sb-avatar.php'),
+            ], 'sb-avatar-config');
+        }
     }
 }
